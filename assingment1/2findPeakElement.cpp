802d58ae8b6a4d4e168645adac97ad022678649b
abhay singh <a8haysingh@gmail.com>
2021-02-15 18:12:04 +0530

    solved third qestion

--- assingment1/2findPeakElement.cpp
+++ assingment1/2findPeakElement.cpp
@@ -3,8 +3,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int findPeakElement(vector<int> vec)
+{
+    int l = 0, r = vec.size(), mid;
+    while (l < r)
+    {
+        mid = (r - l) / 2 + l;
+        if (vec[mid] >= vec[mid + 1] && vec[mid] >= vec[mid - 1])
+        {
+            return mid;
+        }
+        else if (vec[mid + 1] >= vec[mid])
+        {
+            l = mid;
+        }
+        else
+        {
+            r = mid;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
-    cout << "ok" << endl;
+    vector<int> vec = {10, 20, 15, 2, 23, 90, 67};
+    cout << findPeakElement(vec) << endl;
     return 0;
 }
