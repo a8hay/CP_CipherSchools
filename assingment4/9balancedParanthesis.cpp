97be09802b4acb515a17db4ceb7476833c3ab644
abhay singh <a8haysingh@gmail.com>
2021-02-17 16:46:24 +0530

    solved balanced paranthesis

--- assingment4/9balancedParanthesis.cpp
+++ assingment4/9balancedParanthesis.cpp
@@ -1,7 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isBalanced(string input)
+{
+    stack<char> S;
+    for (char c : input)
+    {
+        if (c == '[' || c == '(' || c == '{')
+        {
+            S.push(c);
+        }
+        else
+        {
+            if (S.empty())
+            {
+                return false;
+            }
+            char open = S.top();
+            S.pop();
+            if (open == '(')
+            {
+                if (c != ')')
+                    return false;
+            }
+
+            if (open == '{')
+            {
+                if (c != '}')
+                    return false;
+            }
+
+            if (open == '[')
+            {
+                if (c != ']')
+                    return false;
+            }
+        }
+    }
+    return S.empty();
+}
+
 int main()
 {
+    string input1 = "[()]{}{[()()]()}";
+    string input2 = "[(])";
+    cout << isBalanced(input1) << endl;
+    cout << isBalanced(input2) << endl;
     return 0;
 }
