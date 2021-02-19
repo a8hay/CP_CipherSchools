521dd77730e5701d870ca63a8048b8d2dc45c31c
abhay singh <a8haysingh@gmail.com>
2021-02-19 10:46:47 +0530

    lca and diameter

--- assingment5/4LCArecursiveIterative.cpp
+++ assingment5/4LCArecursiveIterative.cpp
@@ -16,8 +16,23 @@ public:
     }
 };
 
-Node *LCA(Node *root)
+Node *LCA(Node *root, Node *a, Node *b)
 {
+    if (root == NULL)
+        return NULL;
+
+    if (root == a || root == b)
+    {
+        return root;
+    }
+
+    Node *l = LCA(root->left, a, b);
+    Node *r = LCA(root->right, a, b);
+
+    if (l != NULL && r != NULL)
+        return root;
+    else
+        return (l != NULL ? l : r);
 }
 
 int main()
@@ -26,15 +41,15 @@ int main()
     root->left = new Node(2);
     root->right = new Node(3);
     root->right->left = new Node(4);
-    root->right->left->left = new Node(6);
+    Node *a = root->right->left->left = new Node(6);
     root->right->right = new Node(5);
-    root->right->right->left = new Node(7);
+    Node *b = root->right->right->left = new Node(7);
     root->right->right->right = new Node(8);
     root->right->right->right->left = new Node(9);
 
-    Node *lca = LCA(root);
+    Node *lca = LCA(root, a, b);
     if (lca != NULL)
-        cout << "lowest common ancestor" << lca->data << endl;
+        cout << "lowest common ancestor " << lca->data << endl;
     else
         cout << "no lowest common ancestor" << endl;
     return 0;
