7068205895fc27370ee10950aa69fd1aa0ba2d34
abhay singh <a8haysingh@gmail.com>
2021-02-19 10:14:04 +0530

    created file for binary tree assingment

---
+++ assingment5/3leftRightTopBottomView.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int data = 0)
+    {
+        this->data = data;
+        this->right = NULL;
+        this->left = NULL;
+    }
+};
+
+void topView(Node *root)
+{
+}
+
+void rightView(Node *root)
+{
+}
+
+void bottomView(Node *root)
+{
+}
+
+void leftView(Node *root)
+{
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->right->left = new Node(4);
+    root->right->left->left = new Node(6);
+    root->right->right = new Node(5);
+    root->right->right->left = new Node(7);
+    root->right->right->right = new Node(8);
+    root->right->right->right->left = new Node(9);
+    cout << "top" << endl;
+    topView(root);
+    cout << endl;
+    cout << "rigt" << endl;
+    rightView(root);
+    cout << endl;
+    cout << "bottom" << endl;
+    bottomView(root);
+    cout << endl;
+    cout << "left" << endl;
+    leftView(root);
+    cout << endl;
+    return 0;
+}
